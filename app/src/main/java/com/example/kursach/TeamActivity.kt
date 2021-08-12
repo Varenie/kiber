21a954bcf038cbfc1bb30b4507355b35a8469426
Varenie <sanyacorotkih@yandex.ru>
2021-08-12 19:52:42 +0300

    сделано окно добавления игрока

--- app/src/main/java/com/example/kursach/TeamActivity.kt
+++ app/src/main/java/com/example/kursach/TeamActivity.kt
@@ -2,19 +2,46 @@ package com.example.kursach
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class TeamActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_team)
 
+        val fabAdd = findViewById<FloatingActionButton>(R.id.fab_player_add)
+
+        fabAdd.setOnClickListener {
+            openAddDialog()
+        }
         val myRecycler = findViewById<RecyclerView>(R.id.rv_players)
         myRecycler.layoutManager = LinearLayoutManager(this)
         myRecycler.setHasFixedSize(true)
 
         val adapter = PlayersAdapter()
         myRecycler.adapter = adapter
+    }
+
+    private fun openAddDialog() {
+        val dialog = AlertDialog.Builder(this)
+        dialog.setTitle("Добавить игрока")
+
+        val inflater = LayoutInflater.from(this)
+        val addWindow = inflater.inflate(R.layout.layout_player_add, null)
+        dialog.setView(addWindow)
+
+        dialog.setNegativeButton("Отменить") {dialogInterface, which ->
+            dialogInterface.dismiss()
+        }
+
+        dialog.setPositiveButton("Подтвердить") {dialogInterface, which ->
+            dialogInterface.dismiss()
+        }
+
+        dialog.show()
     }
 }
